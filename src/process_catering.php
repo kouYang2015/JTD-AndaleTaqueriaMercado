e7f1c4cdf1e32bfaedffbc00b690310894bad852
Kou <kou.yang2015@gmail.com>
2022-11-12 13:01:52 -0600

    Added template code to html/php files.

---
+++ src/process_catering.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Catering Confirmation</title>
+</head>
+
+<body>
+    <?php
+    echo "TODO: add php script";
+    ?>
+</body>
+
+</html>
---
+++ src/process_order.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Order Confirmation</title>
+</head>
+
+<body>
+    <?php
+    echo "TODO: add php script";
+    ?>
+</body>
+
+</html>
