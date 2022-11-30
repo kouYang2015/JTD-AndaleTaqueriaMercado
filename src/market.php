46b07d26493b2af35714f4f9186616da6a792c65
Kou <kou.yang2015@gmail.com>
2022-11-30 00:09:47 -0600

    header.php and footer.php included in market.php

---
+++ src/market.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Market</title>
+  <link rel="stylesheet" href="./css/market.css" />
+  <link rel="stylesheet" href="./css/theme.css" />
+</head>
+
+<body>
+  <?php include 'header.php'; ?>
+  <div>
+    <h1 style="text-align: center; padding: 30px">Market</h1>
+    <div class="container">
+      <p>
+        Our Mercado (Market) has everything you need to prepare an authentic
+        Mexican meal.
+      </p>
+      <br />
+      <p>
+        We offer fresh vegetables and fruits in our produce section, specalty
+        cut meats at our butcher counter, and a large selection of grocery
+        items
+      </p>
+      <br />
+      <p>
+        We have freshly baked Mexican breads, pastries, and desserts daily.
+      </p>
+      <br />
+      <p>
+        Order your Tres Leches cakes custom made for that special occasion or
+        prepare a quick meal from our hot and cold deli.
+      </p>
+    </div>
+    <div class="slider"><img src="./images/backfilled.png" /></div>
+  </div>
+  <?php include 'footer.php'; ?>
+</body>
+
+</html>
