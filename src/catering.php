8a23537fe387c6f70a6f204932bf0f70985ae8d4
Kou <kou.yang2015@gmail.com>
2022-11-30 01:12:08 -0600

    Info container complete with css.

--- src/catering.php
+++ src/catering.php
@@ -5,12 +5,18 @@
   <title>Catering</title>
   <link rel="stylesheet" href="./css/theme.css" />
   <link rel="stylesheet" href="./css/catering.css" />
+  <link href='https://fonts.googleapis.com/css?family=Annie Use Your Telescope' rel='stylesheet'>
 
 </head>
 
 <body>
   <?php include 'header.php'; ?>
-  <div>Content Here</div>
+  <div class="info-container">
+    <h1 class="title-fonts">Catering</h1>
+    <p>Click button below to make a catering inquiry request.</p>
+    <p>We will send you an estimate cost and time base on your request within 1-3 business days.</p>
+    <a href="catering_form.php"><button type="submit" class="button" value="Make Inquiry">Make Inquiry</button></a>
+  </div>
   <?php include 'footer.php'; ?>
 </body>
 
