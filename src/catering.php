574bb11980b56fb873643a22a94d986008a46746
Kou <kou.yang2015@gmail.com>
2022-12-01 00:16:29 -0600

    Catering page and css complete.

--- src/catering.php
+++ src/catering.php
@@ -17,6 +17,50 @@
     <p>We will send you an estimate cost and time base on your request within 1-3 business days.</p>
     <a href="catering_form.php"><button type="submit" class="button" value="Make Inquiry">Make Inquiry</button></a>
   </div>
+  <div class="catering-menu-container">
+    <h2 class="title-fonts">Entrees</h2>
+    <div class="catering-menu-info ">
+      <p><strong>TACO BAR</strong> &#45; Served with rice, beans, tortillas, pico de gallo, guacamole, chips, salsas, cilantro, limes and onion.</p>
+      <p><strong>FAJITA BAR</strong> &#45; Served with rice, beans, tortillas, fajita veggie mix, pico de gallo, guacamole, chips, salsas, lettuce, cheese, and sour cream.</p>
+      <p><strong>NACHO BAR</strong> &#45; Served with queso, guacamole, sour cream and chips.</p>
+      <p><strong>SALAD</strong>
+      <ul>
+        <li>CACTUS LEAF SALAD &#45; Cactus leaf mixed with fresh cilantro, tomatoes, onions and cotija cheese.</li>
+        <li>HOUSE MEXICAN SALAD &#45; Chopped iceberg lettuce tossed with tomatoes, cucumbers, red onion, cactus leaves, queso fresco and cilantro lime dressing.</li>
+      </ul>
+      </p>
+      <p><strong>ENCHILADA</strong> &#45; Corn tortillas filled with chicken and cheese smothered with red or green salsa.</p>
+      <p><strong>QUESADILLA</strong> &#45; Grilled flour tortilla filled with cheese and/or chicken.</p>
+      <p><strong>TAQUITOS OR FLAUTAS</strong> &#45; Crisp fried rolled taco filled with chicken or pork.</p>
+      <p><strong>&emsp;&#42;HOUSE CHIPS SERVED with GUACAMOLE AND/OR PICO DE GALLO</strong></p>
+    </div>
+    <h2 class="title-fonts">Entree Meat Choice</h2>
+    <div class="catering-menu-info ">
+      <p><strong>AL PASTOR</strong> &#45; Thinly sliced marinated pork.</p>
+      <p><strong>CARNITAS</strong> &#45; Slow cooked pork shoulder.</p>
+      <p><strong>CHORIZO</strong> &#45; House made ground Mexican sausage.</p>
+      <p><strong>GRILLED CHICKEN</strong> &#45; Chicken breast marinated in seasoning and lime.</p>
+      <p><strong>POllO a la MEXICANA</strong> &#45; Shredded chicken cooked with tomatoes, onion and pablano peppers.</p>
+      <p><strong>POLLO RANCHERO</strong> &#45; Shredded chicken cooked in medium Ranchero sauce.</p>
+      <p><strong>CARNE ASADA</strong> &#45; Diced chuck roll steak grilled and seasoned with our house blend.</p>
+      <p><strong>BARBACOA </strong> &#45; Slow cooked beef with traditional Mexican spices and herbs.</p>
+    </div>
+    <h2 class="title-fonts">Traditional Mexican Beverages</h2>
+    <div class="catering-menu-info ">
+      <p><strong>HORCHATA</strong> &#45; Cinnamon rice water.</p>
+      <p><strong>TAMARIND WATER</strong></p>
+      <p><strong>HIBISCUS WATER</strong></p>
+      <p><strong>JARRITOS</strong> &#45; Comes in assorted flavors.</p>
+    </div>
+    <h2 class="title-fonts">Desserts</h2>
+    <div class="catering-menu-info ">
+      <p><strong>TRES LECHES CAKE</strong> &#45; White cake soaked with 3 kinds of milk.</p>
+      <p>&emsp;&#42;Comes in variety of flavor: plain, mocha, piña colada, or fruit filled</p>
+      <p><strong>FLAN </strong> &#45; Mexican custard cake.</p>
+      <p>&emsp;&#42;Neopolitano or Choco Flan.</p>
+      <p><strong>CHURROS</strong></p>
+    </div>
+  </div>
   <?php include 'footer.php'; ?>
 </body>
 
