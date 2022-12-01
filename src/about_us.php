fcca2c3e5bb98fd67cf2e54e84b4bb16b86bfcec
Kou <kou.yang2015@gmail.com>
2022-12-01 00:53:31 -0600

    About us information paragraph

--- src/about_us.php
+++ src/about_us.php
@@ -3,6 +3,7 @@
 
 <head>
   <title>About Us</title>
+  <link href='https://fonts.googleapis.com/css?family=Annie Use Your Telescope' rel='stylesheet'>
   <link rel="stylesheet" href="./css/theme.css" />
   <link rel="stylesheet" href="./css/about_us.css" />
 
@@ -10,7 +11,14 @@
 
 <body>
   <?php include 'header.php'; ?>
-  <div>Content Here</div>
+  <div class="info-container">
+    <h1 class="title-fonts">About Us</h1>
+    <p>We serve all authentic Mexican food that you will find being sold on the streets of Mexico.
+      Our quick-eat Mexican Taqueria (Restaurant) makes food fresh and from scratch daily.
+      Our ingredients come straight from our <i>Mercado</i> (Market) right next door.
+      You won't be disappointed with our tacos, burritos, tortas, Menudo, and Tamales.
+      Order online or give us a call and we will have your food ready for pick up. </p>
+  </div>
   <?php include 'footer.php'; ?>
 </body>
 
