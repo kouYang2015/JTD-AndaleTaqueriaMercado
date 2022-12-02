d0e410b3b2fa83d8db49c5673165738b34bf6209
Kou <kou.yang2015@gmail.com>
2022-12-02 00:42:39 -0600

    Added title, page desc, and container for form

--- src/catering_form.php
+++ src/catering_form.php
@@ -9,7 +9,18 @@
 
 <body>
   <?php include 'header.php'; ?>
-  <div>Content Here</div>
+  <div class="info-container">
+    <h1 class="title-fonts">Catering</h1>
+    <p>This is an inquiry only&#33;</p>
+    <p>We will send you an estimate cost and time based on your request within 1&#45;3 business days.</p>
+  </div>
+  <div class="catering_form-container">
+    <form action="process_catering.php" method="post">
+
+      <input class="button" type=submit name="order" value="Submit" />
+    </form>
+  </div>
+
   <?php include 'footer.php'; ?>
 </body>
 
