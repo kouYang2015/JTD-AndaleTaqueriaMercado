fe80c91aca49de932858acd84d3453f599b9e9bb
Kou Yang <80478758+kouYang2015@users.noreply.github.com>
2022-12-02 10:51:25 -0600

    About Us page (#9)
    * font family for headers now in theme.css
    
    * About us information paragraph
    
    * Slideshow carousel functionality added.
    
    * Credited HTML/CSS/JS for carousel to w3schools
    
    * Centers image horizontally as well in slideshow

--- src/about_us.php
+++ src/about_us.php
@@ -3,14 +3,93 @@
 
 <head>
   <title>About Us</title>
-  <link rel="stylesheet" href="./css/theme.css" />
-  <link rel="stylesheet" href="./css/about_us.css" />
-
+  <link href='https://fonts.googleapis.com/css?family=Annie Use Your Telescope' rel='stylesheet'>
+  <link rel="stylesheet" type="text/css" href="./css/theme.css" />
+  <link rel="stylesheet" type="text/css" href="./css/about_us.css" />
+  <link rel="stylesheet" type="text/css" href="./css/slideshow.css" />
 </head>
 
 <body>
   <?php include 'header.php'; ?>
-  <div>Content Here</div>
+  <div class="info-container">
+    <h1 class="title-fonts">About Us</h1>
+    <p>We serve all authentic Mexican food that you will find being sold on the streets of Mexico.
+      Our quick-eat Mexican Taqueria (Restaurant) makes food fresh and from scratch daily.
+      Our ingredients come straight from our <i>Mercado</i> (Market) right next door.
+      You won't be disappointed with our tacos, burritos, tortas, Menudo, and Tamales.
+      Order online or give us a call and we will have your food ready for pick up. </p>
+  </div>
+
+  <!-- HTML structure based off of https://www.w3schools.com/howto/howto_js_slideshow.asp-->
+  <div class="slideshow-container">
+    <div class="center-img">
+      <div class="img-container fade-anim">
+        <img src="./images/aboutus1.jpg">
+      </div>
+
+      <div class="img-container fade-anim">
+        <img src="./images/aboutus2.jpg">
+      </div>
+
+      <div class="img-container fade-anim">
+        <img src="./images/aboutus3.jpg">
+      </div>
+
+      <div class="img-container fade-anim">
+        <img src="./images/aboutus4.png">
+      </div>
+
+      <div class="img-container fade-anim">
+        <img src="./images/aboutus5.jpg">
+      </div>
+    </div>
+    <span class="prev-arrow" onclick="incrementSlide(-1)">&#10094;</span>
+    <span class="next-arrow" onclick="incrementSlide(1)">&#10095;</span>
+  </div>
+  <br>
+
+  <div class="dot-container">
+    <span class="dot" onclick="jumpslide(1)"></span>
+    <span class="dot" onclick="jumpslide(2)"></span>
+    <span class="dot" onclick="jumpslide(3)"></span>
+    <span class="dot" onclick="jumpslide(4)"></span>
+    <span class="dot" onclick="jumpslide(5)"></span>
+  </div>
+  <script>
+    //JS structure based off of https://www.w3schools.com/howto/howto_js_slideshow.asp
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+      let i = 0;
+      let img_container = document.getElementsByClassName("img-container");
+      let dots = document.getElementsByClassName("dot");
+      if (n > img_container.length) {
+        slideIndex = 1
+      }
+      if (n < 1) {
+        slideIndex = img_container.length
+      }
+      for (i = 0; i < img_container.length; i++) {
+        img_container[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      img_container[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
+
+    // Next/previous control
+    function incrementSlide(n) {
+      showSlides(slideIndex += n);
+    }
+
+    // Dot jump control
+    function jumpslide(n) {
+      showSlides(slideIndex = n);
+    }
+  </script>
   <?php include 'footer.php'; ?>
 </body>
 
