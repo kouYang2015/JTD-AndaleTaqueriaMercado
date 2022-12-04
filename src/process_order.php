ef17371efe15ed3dca6fddb09ff656c8966316ca
marwit0800 <marcus.withers@my.metrostate.edu>
2022-12-04 00:20:11 -0600

    Order page few options | Process order calculation logic for recipt

--- src/process_order.php
+++ src/process_order.php
@@ -4,10 +4,38 @@
 <head>
     <title>Order Confirmation</title>
 </head>
+<?php
+$alambres = $_POST['alambres'];
+$alambres_quantity = $_POST['alambres-quantity'];
+$burritos = $_POST['burritos'];
+$burritos_quantity = $_POST['burritos-quantity'];
+$tacos = $_POST['tacos'];
+$tacos_quantity = $_POST['tacos-quantity'];
+
+$alambres = filter_var($alambres, FILTER_SANITIZE_NUMBER_INT);
+$alambres = $alambres * $alambres_quantity; // Alambres total
+
+$burritos = filter_var($burritos, FILTER_SANITIZE_NUMBER_INT);
+$burritos = $burritos * $burritos_quantity; // Burritos total
+
+$tacos = filter_var($tacos, FILTER_SANITIZE_NUMBER_INT);
+$tacos = $tacos * $tacos_quantity; // Tacos total
+
+$total = $alambres + $burritos + $tacos;
+?>
 
 <body>
     <?php include 'header.html'; ?>
-    <div>Content Here</div>
+    <div>
+        <?php echo "Alambres: " . $alambres; ?>
+        <br />
+        <?php echo "Burritos: " . $burritos; ?>
+        <br />
+        <?php echo "Tacos: " . $tacos; ?>
+        <br />
+        <?php echo "Total: " . $total ?>
+        <br />
+    </div>
     <?php include 'footer.html'; ?>
 </body>
 
