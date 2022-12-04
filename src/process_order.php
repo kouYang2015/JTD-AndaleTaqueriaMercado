c541c739c5a64b60e14c31cc410d2062ce7eff5f
marwit0800 <marcus.withers@my.metrostate.edu>
2022-12-04 00:58:39 -0600

    corrected output formatting

--- src/process_order.php
+++ src/process_order.php
@@ -6,19 +6,21 @@
 </head>
 <?php
 $alambres = $_POST['alambres'];
+$alambres = preg_replace("#[^0-9\.,]#", '', $alambres); // Formats post output to just the numbers and decimal
 $alambres_quantity = $_POST['alambres-quantity'];
+
 $burritos = $_POST['burritos'];
+$burritos = preg_replace("#[^0-9\.,]#", '', $burritos); // Formats post output to just the numbers and decimal
 $burritos_quantity = $_POST['burritos-quantity'];
+
 $tacos = $_POST['tacos'];
+$tacos = preg_replace("#[^0-9\.,]#", '', $tacos); // Formats post output to just the numbers and decimal
 $tacos_quantity = $_POST['tacos-quantity'];
 
-$alambres = filter_var($alambres, FILTER_SANITIZE_NUMBER_INT);
 $alambres = $alambres * $alambres_quantity; // Alambres total
 
-$burritos = filter_var($burritos, FILTER_SANITIZE_NUMBER_INT);
 $burritos = $burritos * $burritos_quantity; // Burritos total
 
-$tacos = filter_var($tacos, FILTER_SANITIZE_NUMBER_INT);
 $tacos = $tacos * $tacos_quantity; // Tacos total
 
 $total = $alambres + $burritos + $tacos;
@@ -27,13 +29,13 @@ $total = $alambres + $burritos + $tacos;
 <body>
     <?php include 'header.html'; ?>
     <div>
-        <?php echo "Alambres: " . $alambres; ?>
+        <?php echo "Alambres: " . "$" . $alambres ?>
         <br />
-        <?php echo "Burritos: " . $burritos; ?>
+        <?php echo "Burritos: " . "$" . $burritos; ?>
         <br />
-        <?php echo "Tacos: " . $tacos; ?>
+        <?php echo "Tacos: " . "$" . $tacos; ?>
         <br />
-        <?php echo "Total: " . $total ?>
+        <?php echo "Total: " . "$" . $total ?>
         <br />
     </div>
     <?php include 'footer.html'; ?>
