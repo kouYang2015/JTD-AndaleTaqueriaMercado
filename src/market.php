58e9c9809e6cb1cf646ae0faa207f0860871c16f
Kou <kou.yang2015@gmail.com>
2022-12-06 20:59:06 -0600

    updated styling of p tag to match aboutus page

--- src/market.php
+++ src/market.php
@@ -12,23 +12,14 @@
   <?php include 'header.html'; ?>
   <div>
     <h1 style="text-align: center; padding: 30px">Market</h1>
-    <div class="container">
+    <div class="info-container">
       <p>
         Our Mercado (Market) has everything you need to prepare an authentic
         Mexican meal.
-      </p>
-      <br />
-      <p>
         We offer fresh vegetables and fruits in our produce section, specalty
         cut meats at our butcher counter, and a large selection of grocery
         items
-      </p>
-      <br />
-      <p>
         We have freshly baked Mexican breads, pastries, and desserts daily.
-      </p>
-      <br />
-      <p>
         Order your Tres Leches cakes custom made for that special occasion or
         prepare a quick meal from our hot and cold deli.
       </p>
